4cdceacc287559dec120c324d8fd6d572114e5d0
jsluzek <45077666+jsluzek@users.noreply.github.com>
2020-10-11 16:57:59 +0200

    Create before_script.py

---
+++ before_script.py
@@ -0,0 +1,21 @@
+from __future__ import print_function
+import sys
+
+
+def hello(what):
+    print('Hello, {}!'.format(what))
+
+
+def say_what():
+    return 'world'
+
+
+def main():
+    hello(say_what())
+    print('xd')
+    print('xdd')
+    return 0
+
+
+if __name__ == '__main__':
+    sys.exit(main())
