cb2d2935b27aa02e844d1d401245eff68bc6b851
jsluzek <45077666+jsluzek@users.noreply.github.com>
2020-10-11 17:15:07 +0200

    Update test_hello.py

--- tests/test_hello.py
+++ tests/test_hello.py
@@ -3,3 +3,14 @@ import hello
 
 def test_says_world():
     assert hello.say_what() == 'world'
+def test_says_world1():
+    assert 1 == 1
+    
+def test_says_world2():
+    assert 2 == 2
+    
+def test_says_world3():
+    assert 3 == 3
+    
+def test_says_world4():
+    assert 4 == 4
